f91b74ee4b147049210483a9a28601df677aa809
Miguel Álvarez Herrera <m.alvarez.herrera@csic.es>
2024-04-25 15:37:11 +0200

    Fix edge thickness and add the option to omit metabolites

--- modelling/plotnet.py
+++ modelling/plotnet.py
@@ -33,55 +33,58 @@ class NetPlotter:
                 self.g.add_node(product.id, bipartite=self.METABOLITE)
                 self.g.add_edge(reaction.id, product.id)
     
-    def _select_all_nodes(self) -> Tuple[set, set]:
+    def _select_all_nodes(self, omit_metabolites: set) -> Tuple[set, set]:
         reaction_nodes = {n for n, d in self.g.nodes(data=True) if d["bipartite"] == self.REACTION}
-        metabolite_nodes = set(self.g) - reaction_nodes
+        metabolite_nodes = set(self.g) - reaction_nodes - omit_metabolites
         return metabolite_nodes, reaction_nodes
     
-    def _select_nodes_with_reactions(self, reaction_ids: set) -> Tuple[set, set]:
+    def _select_nodes_with_reactions(self, reaction_ids: set, omit_metabolites: set) -> Tuple[set, set]:
         reaction_nodes = {n for n, d in self.g.nodes(data=True) if (d["bipartite"] == self.REACTION) and (n in reaction_ids)}
         metabolite_nodes = set()
         for reaction in (reaction for reaction in self.model.reactions if reaction.id in reaction_nodes):
             for metabolite in reaction.metabolites:
                 metabolite_nodes.add(metabolite.id)
-        return metabolite_nodes, reaction_nodes
+        return metabolite_nodes - omit_metabolites, reaction_nodes
 
-    def plot(self, reaction_ids: set = {}, layout_func: Callable[[nx.Graph], Mapping] = None) -> None:
-        if layout_func is not None:
-            position = layout_func(self.g)
-        else:
-            position = nx.kamada_kawai_layout(self.g)
+    def plot(self, reaction_ids: set = set(), omit_metabolites: set = set(), layout_func: Callable[[nx.Graph], Mapping] = None) -> None:
         # Get types of nodes
         if len(reaction_ids) == 0:
-            metabolite_nodes, reaction_nodes = self._select_all_nodes()
+            metabolite_nodes, reaction_nodes = self._select_all_nodes(omit_metabolites)
             g = self.g
         else:
-            metabolite_nodes, reaction_nodes = self._select_nodes_with_reactions(reaction_ids)
-            g = self.g.subgraph(list(reaction_nodes | metabolite_nodes))
+            metabolite_nodes, reaction_nodes = self._select_nodes_with_reactions(reaction_ids, omit_metabolites)
+            g = self.g.subgraph(list((reaction_nodes | metabolite_nodes) - omit_metabolites))
+        # Set layout
+        if layout_func is not None:
+            position = layout_func(g)
+        else:
+            position = nx.kamada_kawai_layout(g)
         # Plot nodes
         nx.draw_networkx_nodes(g, position, nodelist=list(reaction_nodes), node_color="tab:blue", **PLOT_OPTIONS)
         nx.draw_networkx_nodes(g, position, nodelist=list(metabolite_nodes), node_color="tab:orange", **PLOT_OPTIONS)
         nx.draw_networkx_labels(g, position, font_size=10, font_color="black")
         # Plot solid edges
-        nx.draw_networkx_edges(g, position, width=1.0, alpha=1., connectionstyle=EDGE_STYLE)
+        nx.draw_networkx_edges(g, position, width=1.0, alpha=1.0, connectionstyle=EDGE_STYLE)
         # Plot flux over edges
         solution = self.model.optimize()
-        nonzero_fluxes = solution.fluxes[solution.fluxes.abs() > ZERO]
+        solution_filtered = solution[list(reaction_nodes)]
+        nonzero_fluxes = solution_filtered[solution_filtered.abs() > ZERO]
         if nonzero_fluxes.max() != nonzero_fluxes.min():
             scaled_fluxes = EDGE_WIDTH_MIN + (EDGE_WIDTH_MAX - EDGE_WIDTH_MIN) * (nonzero_fluxes - nonzero_fluxes.min()) / (nonzero_fluxes.max() - nonzero_fluxes.min())
         else:
             scaled_fluxes = (nonzero_fluxes - nonzero_fluxes) + EDGE_WIDTH_MAX
         for reaction_id, width in scaled_fluxes.items():
-            nx.draw_networkx_edges(
-                g,
-                position,
-                edgelist=set(g.out_edges(reaction_id)) | set(g.in_edges(reaction_id)),
-                width=width,
-                alpha=0.5,
-                edge_color="tab:blue",
-                connectionstyle=EDGE_STYLE,
-                arrowstyle="-"
-            )
+            if reaction_id in reaction_nodes:
+                nx.draw_networkx_edges(
+                    g,
+                    position,
+                    edgelist=set(g.out_edges(reaction_id)) | set(g.in_edges(reaction_id)),
+                    width=width,
+                    alpha=0.5,
+                    edge_color="tab:blue",
+                    connectionstyle=EDGE_STYLE,
+                    arrowstyle="-"
+                )
         # Output plot
         plt.tight_layout()
         plt.axis("off")
